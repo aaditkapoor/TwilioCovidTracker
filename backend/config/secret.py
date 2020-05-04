e7665f8401e22b2b3adc3d8999b22e07bbf73b0f
aaditkapoor <akapoorx00@gmail.com>
2020-05-04 06:33:16 +0530

    pushed on heroku

--- backend/config/secret.py
+++ backend/config/secret.py
@@ -1,4 +1,3 @@
-# Download the helper library from https://www.twilio.com/docs/python/install
 import os
 import datetime
 import logging
@@ -6,9 +5,9 @@ import logging
 # Your Account Sid and Auth Token from twilio.com/console
 # DANGER! This is insecure. See http://twil.io/secure
 KEYS = {}
-KEYS["account_sid"] = 'AC3d1c37040cbb5a88673e3f7e46e5f9d4'
-KEYS["auth_token"] = '8232af2bd0f4e31b53e0e9ed42fa7d54'
-KEYS["from_"] = "+19382010923"
+KEYS["account_sid"] = '<Add_your_sid>'
+KEYS["auth_token"] = '<Add_your_token>'
+KEYS["from_"] = "<paste_phone_number"
 KEYS["to"] = ""
 
 
